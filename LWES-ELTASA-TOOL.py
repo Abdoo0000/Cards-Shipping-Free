73b281f5889a66746abb840b7865c39923c4dec8
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2021-03-12 14:56:56 +0200

    Create LWES-ELTASA-TOOL.py

---
+++ LWES-ELTASA-TOOL.py
@@ -0,0 +1,68 @@
+import os
+import time
+os.system("clear")
+
+v = "lozer"
+password = input("password : ")
+if password == v :
+    print ("loding..")
+else :
+    kosmak
+    
+os.system ("clear")
+time.sleep(2)
+print ("\033[0;33m Made Py »LWES ELTASA HACK«")
+print ("\033[0;33m WA »01283494525«")
+print ("\033[0;33m Youtube »LWES ELTASA HACK")
+#colors
+print ("\033[0;32mMMMMMMMMMMMMNho++/----:+syydmMMMMMMMMMMM")
+print ("\033[0;32mMMMMMMMMh+:.                 `+mMMMMMMMM")
+print ("\033[0;32mMMMMMNs-           .` `    `..` -+mMMMMM")
+print ("\033[0;32mMMMMm- `:+/+:``   ./-.``:ohmNmmdo.`yMMMM")
+print ("\033[0;32mMMMy``/yhdmmmmmmy+---:smNNNNNNNmmd/`/NMM")
+print ("\033[0;32mMM+ .ohdmmNNNNNNNNNNNNNNNNNNNNNNNmm:`-NM")
+print ("M/ `oydmmmNNNNNNNNNNNNNNNNNNNNNNNNmd-`+M")
+print ("m  :shdyymmNNNNNNNNNNNNNNNNNNNds+smm+  m")
+print ("h  /y:`  `:smNNNNNNNNNNNNNNNh:`.-``ss  y")
+print ("d  o-`/hdy+-.+mNNNNNNNNNNNs:-+dNNm+.s  s")
+print ("s``+`sdmmNNNy:-smNNNNNNNy::odNNNNNNo:. s")
+print ("s`-:oddddddddms::mNNNNNo.:ydddmmmNNm-+ y")
+print ("s`+syhddmmmmdyyhymNNNNNmhhdmNNNNNNmmhd`y")
+print ("o-sssydNMNdmNNmhyhmNNNmhdmmNmNMMMNmmmm/")
+print ("NshddymMM:`.dMNmhydNNmdmmNM:.`mMMMmNNmdm")
+print ("mydmmddmNNdmMMMNdhdNNmmNNNNNddMMMNNNNNmM")
+print ("mydmmmy.hNNNNNNNdhdNNmmNNNNNNNNysmNNNNmM")
+print ("Myhdmmm/-dmNNNmmhhmNNmmNNNMMNNN:hNNNNNNM")
+print ("Mmoyhdd+:mNNNmmdyydNNmmNNNNNMNN+sNNNmmMM")
+print ("MMhoyhh`ommmmddhyydNNmmmNNNNNNNs-NmmdMMM")
+print ("MMMsos+`dmddddhyyhmNNNmmNNNNNNNd`hmdNMMM")
+print ("MMMN+o/-dddddhyyydNNNNNmNNNNNNNm`sddMMMM")
+print ("MMMMho/-ddhhhyo+sdmNNNmddmNNNNNm.ydNMMMM")
+print ("MMMMdos..-:-.```./oyys+-.::////::mdNMMMM")
+print ("MMMMmoss/-.````-:/++++//::::--/ommmMMMMM")
+print ("MMMMMyssssss+:---:/+++oydmNNNNNNNmmMMMMM")
+print ("MMMMMNsssyyys++oossyyyyyyydNNNNNmmNMMMMM")
+print ("MMMMMMNysyyhddddmNNNNNNNNNNNNNNmNMMMMMMM")
+print ("MMMMMMMMmyyhmmmNNNNNNMMMNNNNNNNMMMMMMMMM")
+print ("MMMMMMMMMNhydmmNNNNNNNNNNNNNNNMMMMMMMMMM")
+print ("MMMMMMMMMMMNdhdmmmNNNNNNNNNNMMMMMMMMMMMM")
+print ("MMMMMMMMMMMMMMNmmmmmmmNNNMMMMMMMMMMMMMM")
+
+
+print ("\033[0;31m 1-orange")
+print ("\033[0;31m 2-vodafon")
+
+
+m = input("inter numper: ")
+if m != 2 :
+	x = 7502978081578814
+while x < 9999999999999999:
+	x=x+11
+	print (x)
+	time.sleep(2)
+else :
+		n = 8029725739715728
+while n < 8888888888888888 :
+			n=n+14
+			print (n)
+			time.sleep(2)
