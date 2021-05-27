ddcdcb892e87114609a31915fc57380bba2b0d55
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2021-05-28 01:03:09 +0200

    Update run.py

---
+++ run.py
@@ -0,0 +1,89 @@
+import os
+import time
+import webbrowser
+os.system("clear")
+
+v = "LEH&&SHG"
+password = input("password : ")
+if password == v :
+    print ("\033[0;91mlove-you-to ≈)")
+    time.sleep(1)
+else :
+    kosmak
+    
+os.system ("clear")
+time.sleep(2)
+ 
+print ("\033[0;31m    MMMMMMMMMMNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmNNNNNNNNN")
+print ("\033[0;31m    MMMMMMNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNMMMMMM")
+print ("\033[0;31m    MMMMMNNNNNNNmNNNNNNNMNNNNNNNNNNNNNNNNNNNMMMNNMMMMM")
+print ("\033[0;31m    MMMNNNNNNNNmhdhhyhddmmmmmmmddmmmmmmmmmNNNNNNNmNMMM")
+print ("\033[0;31m    MMMNNNNmmNNdhhssmdhhmddNdhhdNNNmhyyhdddmmmmmmNNMMM")
+print ("\033[0;31m    MMNNNNNmmmmmyoohNmNNNmNMNNNNNNNNdoosyhhhdmNNmNNNMM")
+print ("\033[0;31m    MNNNNNmddhhyssdNNNMMNNmdddmNNNNNmhosyhhddNNNNNNNNM")
+print ("\033[0;31m    MNNNmmmmhyysoymMNNNNmhs+oyhmmmNNmhyoshdmmNNmNNNmmM")
+print ("\033[0;31m    MNNNmddddhysohmNNmdhhso+osyhhhdmdymshdddmNNNNmmmNM")
+print ("\033[0;31m    NNNNmmmmdhhshdNmhss++++oo++osyhdmdmdyyhddmNNmdmNMN")
+print ("\033[0;31m    NMNmmNmdhysommmNNNNmdhyyyydmNNNNNmdNsoshdmNmddNNMN")
+print ("\033[0;31m    NMMmdddhyo+ydmmNNNNNMmdNNddNNNNNmhdNdoshddmhdmNNMN")
+print ("\033[0;31m    NMMNmhhyso+mhdmNNNmdysmmhmoossyhdmmmmhoyhhhhmNNNMN")
+print ("\033[0;31m    NMMNNdhhsyhmhdhsoo++++ssos+o++++sddddmooshymNNNNMN")
+print ("\033[0;31m    NMMNNNdyysmmddyssyhhsosoos+sshhhhdhdhmyshhmNNNNNMN")
+print ("\033[0;31m    NMMNNNmdhhmmmmhdmmmdsshsyysysmNNNmhhymdhddNNNNMMMN")
+print ("\033[0;31m    NMMNNNmmdmmmmNNNNNNdhys+oooshmMNNNmhyddmdmmNNNMMMN")
+print ("\033[0;31m    NMMNMNNmmmmmmNNNNNNmhsoooosyhNMNNNNdhddmmmNMMMMMMN")
+print ("\033[0;31m    NMMMNMNNNmmmNmNNNNNNdyo++osydNMNNNNdydddNNNMNNMMMN")
+print ("\033[0;31m    NMMMNMNNNmmmNNNNmNNNdyoooosydNNNNNNNhhhdmNNMNMMMMN")
+print ("\033[0;31m    NMMMNMMmmddmNMNNNNNmdddmNmmddmNNNNNNNhddmmMMNMMMMN")
+print ("\033[0;31m    NMMMMMNmmddNNNNNNmmmNNNNNNNNNmmNmNNNMmdddNNMNMMMMN")
+print ("\033[0;31m    NMMMNNmmmmmMMMMMNmmNNNNNNNNNNNNmNNNNMNdmhNmNNNMMMN")
+print ("\033[0;31m    NMMMNmmmmmmMNMMMNNNNNMNmmmNNNNMNNNNNNNhmhmNmNMMMMN")
+print ("\033[0;31m    NMMMmdmmmdmMNNMMNNNMMNNmddmNNNMMNNMNNMmmhdNmmMMMMN")
+print ("\033[0;31m    NMMNdmdmdNMMNNMMNNMNNNNmmmmNNNMMMMMNMMNmhhNmdNMMMN")
+print ("\033[0;31m    NMMmmmdmmMMNNNMNNMMMNNmmmmmNNMMMMMMNNMMNmhmmmdNMMN")
+print ("\033[0;31m    NMNdmmmNmMMNNNNNNMMMMNNmmmNNNMMNMMMMMMMMMNmmmmNMMN")
+print ("\033[0;31m    NMNdNmmNmMMNNNNNNNNNNNmdhdmNNMMNNMMMMMMMMNNmNmmMMN")
+print ("\033[0;31m    NMNmNmNNNMMMNNNNNNNNNNmdhdmNNNNNNMMMMMMMMNNmNmNMMN")
+print ("\033[0;31m    NMNmNNNNNMMMNNNNNMMMMNmmmmmNNNNMMMMMNMMMMMNmNNNMMN")
+print ("\033[0;31m    NMMNNNNmMMMMMMNNNMMMMNNmdmNNMMMMMMNNNMMMMMNNMNNMMN")
+print ("\033[0;31m    NMMMNNNmNMMMMMNNNNMMMMNNmmNMMMMMNNNNNMMMMMNMMMMMMN")
+print ("\033[0;31m    MMMMNNMNmNNNNNNNNNNNNNNNNNNNNNmNNNNNNNMMMMNMMMMMMd")
+print ("\033[0;31m    MNMMNNMNNmNNNNNNNNNNmmhdNmhhhmmNNNNNNNMMNNMMMMMMN.")
+print ("\033[0;31m    MNMMNMMMNNNNNNNNNNmmmmmmmmNNdmNNNNNNNNNNNMMMMMMMN.")
+print ("\033[0;31m    MMNMMMNMNNNNNNNNNNNNNNMmmmmNNMMNMMMNNNNMMMMMMMMNM.")
+print ("\033[0;31m    MMMNMMMMMMMMMMNNNNMMMMNNmmNNNNMMMMMMMMMMMMMMMMNMM.")
+print ("\033[0;31m    MMMNNMMMMMMMMMMMMMMMMNNNNmNNNNMMMMMMMMMMMMMMMNNMM.")
+print ("\033[0;31m    MMMMMNMMMMMMMMMMMMMNMMMNMNMMMMMMMMMMMMMMMMMMNMMMM.")
+print ("\033[0;31m    MMMMMMNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNMMMMM.")
+print ("MMMMMMMMMMNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmNNNNNNNN.")
+
+print ('''\033[0;92m div by :
+    	$$$$$$$$$$$$$$$$$$$$$$$
+    	$ LWES ELTASA HACK    $
+    	$     A     N     D   $
+    	$ SHADO GABROT ELKBER $
+    	$$$$$$$$$$$$$$$$$$$$$$$
+    	
+	1 » orange
+	2 » vodafon
+	3 » L.E YouTube serch : (LWES ELTASA ELKBER)
+	4 » S.G YouTube serch : (SHADO GABROT)
+	''')
+
+
+m = input("\033[0;36minter your num )» ")
+
+if m != 2 :
+	x = 7281822063191102
+	c=0
+while x < 9999999999999999:
+	x=x+11
+	c=c+1
+	print ("\033[0;92Card num" , c , "»" ,x)
+	time.sleep(0.2) 
+else :
+		n = 8029725739715728
+while n < 8888888888888888 :
+			n=n+14
+			print (n)
+			time.sleep(2)
